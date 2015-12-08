eb30489d74b08b0d6c17acfb81f66fa34202002b
Nguyễn Hoàng Thanh <nguyenhoangthanhsgu@gmail.com>
2015-12-09 00:34:18 +0700

    nothing

--- LTHD_MVC/Controllers/QuanLyController.cs
+++ LTHD_MVC/Controllers/QuanLyController.cs
@@ -1,6 +1,9 @@
-﻿using LTHD_MVC.Models;
+﻿using LTHD_MVC.Helper;
+using LTHD_MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,7 +30,62 @@ namespace LTHD_MVC.Controllers
 
         public ActionResult ThemSanPham()
         {
+            using (LTHD_WebLaptopEntities db = new LTHD_WebLaptopEntities())
+            {
+                List<NhaCungCap> ListNhaCungCap = db.NhaCungCap.ToList();
+                ViewBag.ListNhaCungCap = ListNhaCungCap;
+            }
             return View();
+        }
+
+        public ActionResult XuLyThemSanPham(FormCollection fc)
+        {
+            Session["Status"] = 0;
+            try
+            {
+                using (LTHD_WebLaptopEntities db = new LTHD_WebLaptopEntities())
+                {
+                    SanPham sp = new SanPham();
+                    sp.TenSP = fc["tensanpham"].ToString();
+                    //sp.NhaCungCap = db.NhaCungCap.Find(Int32.Parse(fc["nhacungcap"].ToString()));
+                    sp.Id_NCC = Int32.Parse(fc["nhacungcap"].ToString());
+                    sp.HDD = fc["hdd"].ToString();
+                    sp.RAM = fc["ram"].ToString();
+                    sp.CPU = fc["cpu"].ToString();
+                    sp.DonGia = Double.Parse(fc["dongia"].ToString());
+
+                    HttpPostedFileBase hinh = Request.Files["hinh"];
+                    if ((hinh != null) && (hinh.ContentLength > 0) && !string.IsNullOrEmpty(hinh.FileName))
+                    {
+                        string fileName = StringHelper.DoiTenFile(hinh.FileName);
+                        hinh.SaveAs(Server.MapPath("/Hinh/MayTinh/sanpham/" + fileName));
+                        sp.HinhAnh = fileName;
+                    }
+                    sp.BaoHanh = fc["baohanh"].ToString() + " tháng";
+                    sp.SoLuong = Int32.Parse(fc["soluong"].ToString());
+                    sp.TrangThai = fc["trangthai"].ToString() == "on" ? 1 : 0;
+
+                    db.SanPham.Add(sp);
+                    db.SaveChanges();
+
+                    Session["Status"] = 1;
+                    return RedirectToAction("SanPham");
+                }
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}",
+                                                validationError.PropertyName,
+                                                validationError.ErrorMessage);
+                    }
+                }
+                Session["Status"] = -1;
+                return RedirectToAction("ThemSanPham");
+            }
         }
 
 
---
+++ LTHD_MVC/Helper/StringHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace LTHD_MVC.Helper
+{
+    public class StringHelper
+    {
+        public static string DoiTenFile(String TenFileFull)
+        {
+            string PhanMoRong = Path.GetExtension(TenFileFull);
+            string TenTapTin = Path.GetFileNameWithoutExtension(TenFileFull);
+            string NgayGioHienTai = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+            string FileName = RemoveSpecialCharacters(String.Format("{0}_{1}{2}", TenTapTin, NgayGioHienTai, PhanMoRong));
+            FileName = RemoveSpace(FileName);
+            return FileName;
+        }
+        public static String RemoveSpecialCharacters(String strInput)
+        {
+            try
+            {
+                if (!Object.Equals(strInput, null))
+                {
+                    List<String> ListSpecialCharacters = new List<String>(new String[] { "/", "\\", ":", "*", "?", "\"", "<", ">", "|" });
+                    foreach (String strSpecial in ListSpecialCharacters)
+                    {
+                        strInput = strInput.Replace(strSpecial, String.Empty);
+                    }
+                    return strInput;
+                }
+                return "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static String RemoveSpace(String strInput)
+        {
+            try
+            {
+                if (!Object.Equals(strInput, null))
+                {
+                    strInput.Trim();
+                    strInput = strInput.Replace(" ", String.Empty);
+                    return strInput;
+                }
+                return "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
